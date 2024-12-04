95a815113d3fa0bb3683683b2904982e4f0e00b5
ChayanPandit <chayanpandit07@gmail.com>
2024-12-04 15:19:34 +0530

    env�

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import sanity from '@sanity/astro';
+import { loadEnv } from "vite";
+
+const { SANITY_PROJECT_ID } = loadEnv(process.env.NODE_ENV ?? "", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sanity({
-    projectId: "befnea1j",
+    projectId: SANITY_PROJECT_ID,
     dataset: "production",
     useCdn: true
   })]
